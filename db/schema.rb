58cebb23308d48fa83c534c60cd49d612e25936b
Matt Taube <mctaube@users.noreply.github.com>
2017-06-06 18:00:39 -0500

    updates help from Uday

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606041935) do
+ActiveRecord::Schema.define(version: 20170606172246) do
 
   create_table "residents", force: :cascade do |t|
     t.string   "first_name"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20170606041935) do
     t.string   "last_name"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "mindbody_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
